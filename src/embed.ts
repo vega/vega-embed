a9ef20f4397ec569106471aac523aab1b26a70f5
Dominik Moritz <domoritz@gmail.com>
2017-10-18 11:55:27 -0700

    Catch any error in embed and reject promise.

--- src/embed.ts
+++ src/embed.ts
@@ -30,150 +30,149 @@ const PREPROCESSOR = {
  * @param opt       A JavaScript object containing options for embedding.
  */
 const embed: Export = (el: Element, spec: any, opt: Options) => {
-  opt = opt || {};
-  const actions  = opt.actions !== undefined ? opt.actions : true;
+  try {
+    opt = opt || {};
+    const actions  = opt.actions !== undefined ? opt.actions : true;
 
-  const loader: Loader = opt.loader || vega.loader();
-  const renderer = opt.renderer || 'canvas';
-  const logLevel = opt.logLevel || vega.Warn;
+    const loader: Loader = opt.loader || vega.loader();
+    const renderer = opt.renderer || 'canvas';
+    const logLevel = opt.logLevel || vega.Warn;
 
-  // Load the visualization specification.
-  if (vega.isString(spec)) {
-    return loader.load(spec).then(
-      data => embed(el, JSON.parse(data), opt),
-    ).catch(Promise.reject);
-  }
-
-  // Load Vega theme/configuration.
-  const config = opt.config;
-  if (vega.isString(config)) {
-    return loader.load(config).then(data => {
-      opt.config = JSON.parse(data);
-      return embed(el, spec, opt);
-    }).catch(Promise.reject);
-  }
-
-  // Decide mode
-  let parsed: {library: string, version: string};
-  let mode: Mode;
-
-  if (spec.$schema) {
-    parsed = schemaParser(spec.$schema);
-    if (opt.mode && opt.mode !== parsed.library) {
-      console.warn(`The given visualization spec is written in ${NAMES[parsed.library]}, but mode argument sets ${NAMES[opt.mode]}.`);
+    // Load the visualization specification.
+    if (vega.isString(spec)) {
+      return loader.load(spec).then(
+        data => embed(el, JSON.parse(data), opt),
+      ).catch(Promise.reject);
     }
 
-    mode = parsed.library as Mode;
-
-    if (versionCompare(parsed.version, VERSION[mode]) > 0) {
-      console.warn(`The input spec uses ${mode} ${parsed.version}, but the current version of ${NAMES[mode]} is ${VERSION[mode]}.`);
+    // Load Vega theme/configuration.
+    const config = opt.config;
+    if (vega.isString(config)) {
+      return loader.load(config).then(data => {
+        opt.config = JSON.parse(data);
+        return embed(el, spec, opt);
+      }).catch(Promise.reject);
     }
-  } else {
-    mode = opt.mode || 'vega';
-  }
 
-  let vgSpec = PREPROCESSOR[mode](spec);
-  if (mode === 'vega-lite') {
-    if (vgSpec.$schema) {
-      parsed = schemaParser(vgSpec.$schema);
+    // Decide mode
+    let parsed: {library: string, version: string};
+    let mode: Mode;
 
-      if (versionCompare(parsed.version, VERSION.vega) > 0) {
-        console.warn(`The compiled spec uses Vega ${parsed.version}, but current version is ${VERSION.vega}.`);
+    if (spec.$schema) {
+      parsed = schemaParser(spec.$schema);
+      if (opt.mode && opt.mode !== parsed.library) {
+        console.warn(`The given visualization spec is written in ${NAMES[parsed.library]}, but mode argument sets ${NAMES[opt.mode]}.`);
+      }
+
+      mode = parsed.library as Mode;
+
+      if (versionCompare(parsed.version, VERSION[mode]) > 0) {
+        console.warn(`The input spec uses ${mode} ${parsed.version}, but the current version of ${NAMES[mode]} is ${VERSION[mode]}.`);
+      }
+    } else {
+      mode = opt.mode || 'vega';
+    }
+
+    let vgSpec = PREPROCESSOR[mode](spec);
+    if (mode === 'vega-lite') {
+      if (vgSpec.$schema) {
+        parsed = schemaParser(vgSpec.$schema);
+
+        if (versionCompare(parsed.version, VERSION.vega) > 0) {
+          console.warn(`The compiled spec uses Vega ${parsed.version}, but current version is ${VERSION.vega}.`);
+        }
       }
     }
-  }
 
-  // ensure container div has class 'vega-embed'
-  const div = d3.select(el)
-    .classed('vega-embed', true)
-    .html(''); // clear container
+    // ensure container div has class 'vega-embed'
+    const div = d3.select(el)
+      .classed('vega-embed', true)
+      .html(''); // clear container
 
-  if (opt.onBeforeParse) {
-    // Allow Vega spec to be modified before being used
-    vgSpec = opt.onBeforeParse(vgSpec);
-  }
+    if (opt.onBeforeParse) {
+      // Allow Vega spec to be modified before being used
+      vgSpec = opt.onBeforeParse(vgSpec);
+    }
 
-  let runtime;
-  try {
-    runtime = vega.parse(vgSpec, opt.config);  // may throw an Error if parsing fails
+    const runtime = vega.parse(vgSpec, opt.config);  // may throw an Error if parsing fails
+
+    const view = new vega.View(runtime)
+      .renderer(renderer)
+      .logLevel(logLevel)
+      .loader(loader)
+      .initialize(el);
+
+    // Vega-Lite does not need hover so we can improve perf by not activating it
+    if (mode !== 'vega-lite') {
+      view.hover();
+    }
+
+    if (opt) {
+      if (opt.width) {
+        view.width(opt.width);
+      }
+      if (opt.height) {
+        view.height(opt.height);
+      }
+      if (opt.padding) {
+        view.padding(opt.padding);
+      }
+    }
+
+    view.run();
+
+    if (actions !== false) {
+      // add child div to house action links
+      const ctrl = div.append('div')
+        .attr('class', 'vega-actions');
+
+      // add 'Export' action
+      if (actions === true || actions.export !== false) {
+        const ext = renderer === 'canvas' ? 'png' : 'svg';
+        ctrl.append('a')
+          .text(`Export as ${ext.toUpperCase()}`)
+          .attr('href', '#')
+          .attr('target', '_blank')
+          .attr('download', (spec.name || 'vega') + '.' + ext)
+          .on('mousedown', function(this: HTMLLinkElement) {
+            view.toImageURL(ext).then(url => {
+              this.href =  url;
+            }).catch(error => { throw error; });
+            d3.event.preventDefault();
+          });
+      }
+
+      // add 'View Source' action
+      if (actions === true || actions.source !== false) {
+        ctrl.append('a')
+          .text('View Source')
+          .attr('href', '#')
+          .on('click', () => {
+            viewSource(JSON.stringify(spec, null, 2), opt.sourceHeader || '', opt.sourceFooter || '');
+            d3.event.preventDefault();
+          });
+      }
+
+      // add 'Open in Vega Editor' action
+      if (actions === true || actions.editor !== false) {
+        const editorUrl = opt.editorUrl || 'https://vega.github.io/editor/';
+        ctrl.append('a')
+          .text('Open in Vega Editor')
+          .attr('href', '#')
+          .on('click', () => {
+            post(window, editorUrl, {
+              mode,
+              spec: JSON.stringify(spec, null, 2),
+            });
+            d3.event.preventDefault();
+          });
+      }
+    }
+
+    return Promise.resolve({view, spec});
   } catch (err) {
     return Promise.reject(err);
   }
-
-  const view = new vega.View(runtime)
-    .renderer(renderer)
-    .logLevel(logLevel)
-    .loader(loader)
-    .initialize(el);
-
-  // Vega-Lite does not need hover so we can improve perf by not activating it
-  if (mode !== 'vega-lite') {
-    view.hover();
-  }
-
-  if (opt) {
-    if (opt.width) {
-      view.width(opt.width);
-    }
-    if (opt.height) {
-      view.height(opt.height);
-    }
-    if (opt.padding) {
-      view.padding(opt.padding);
-    }
-  }
-
-  view.run();
-
-  if (actions !== false) {
-    // add child div to house action links
-    const ctrl = div.append('div')
-      .attr('class', 'vega-actions');
-
-    // add 'Export' action
-    if (actions === true || actions.export !== false) {
-      const ext = renderer === 'canvas' ? 'png' : 'svg';
-      ctrl.append('a')
-        .text(`Export as ${ext.toUpperCase()}`)
-        .attr('href', '#')
-        .attr('target', '_blank')
-        .attr('download', (spec.name || 'vega') + '.' + ext)
-        .on('mousedown', function(this: HTMLLinkElement) {
-          view.toImageURL(ext).then(url => {
-            this.href =  url;
-          }).catch(error => { throw error; });
-          d3.event.preventDefault();
-        });
-    }
-
-    // add 'View Source' action
-    if (actions === true || actions.source !== false) {
-      ctrl.append('a')
-        .text('View Source')
-        .attr('href', '#')
-        .on('click', () => {
-          viewSource(JSON.stringify(spec, null, 2), opt.sourceHeader || '', opt.sourceFooter || '');
-          d3.event.preventDefault();
-        });
-    }
-
-    // add 'Open in Vega Editor' action
-    if (actions === true || actions.editor !== false) {
-      const editorUrl = opt.editorUrl || 'https://vega.github.io/editor/';
-      ctrl.append('a')
-        .text('Open in Vega Editor')
-        .attr('href', '#')
-        .on('click', () => {
-          post(window, editorUrl, {
-            mode,
-            spec: JSON.stringify(spec, null, 2),
-          });
-          d3.event.preventDefault();
-        });
-    }
-  }
-
-  return Promise.resolve({view, spec});
 };
 
 function viewSource(source: string, sourceHeader: string, sourceFooter: string) {
