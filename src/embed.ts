b1b08dbed5bc96d3a48c55020e380fdc3a222266
g3o2 <g3o2@users.noreply.github.com>
2018-12-23 21:58:30 +0100

    implement url string for `patch`
    supposed to fix #117

--- src/embed.ts
+++ src/embed.ts
@@ -41,7 +41,7 @@ export interface EmbedOptions {
   loader?: Loader | LoaderOptions;
   renderer?: Renderers;
   tooltip?: TooltipHandler | TooltipOptions | boolean;
-  patch?: PatchFunc | DeepPartial<VgSpec>;
+  patch?: string | PatchFunc | DeepPartial<VgSpec>;
   onBeforeParse?: PatchFunc; // for backwards compatibility
   width?: number;
   height?: number;
@@ -244,6 +244,9 @@ export default async function embed(
   if (patch) {
     if (patch instanceof Function) {
       vgSpec = patch(vgSpec);
+    } else if (patch instanceof String) {
+      const loaded_patch = await loader.load(patch);
+      vgSpec = mergeDeep(vgSpec, JSON.parse(loaded_patch));
     } else {
       vgSpec = mergeDeep(vgSpec, patch);
     }
