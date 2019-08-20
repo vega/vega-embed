0a78a275e3f5e5cadc20cbcea0b5708bb93fce30
Dan Marshall <danmar@microsoft.com>
2019-08-20 10:59:46 -0700

    define MessageData interface

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,13 @@
-import { Config as VgConfig } from 'vega';
+import { Config as VgConfig, Renderers } from 'vega';
 import { Config as VlConfig } from 'vega-lite/build/src/config';
 
 export type Mode = 'vega' | 'vega-lite';
 export type Config = VlConfig | VgConfig;
+
+export interface MessageData {
+    spec: string;
+    file: unknown;
+    config: Config;
+    mode: Mode;
+    renderer: Renderers;
+}
