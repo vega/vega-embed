85eea1b3dc5b0fe3e85284a27b636c91276b7a02
Dominik Moritz <domoritz@gmail.com>
2019-10-07 11:22:01 -0700

    Support JSON patch, use mergeConfig (#235)

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,4 @@
-export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]> };
+import { writeConfig } from 'vega';
 
 // polyfill for IE
 if (!String.prototype.startsWith) {
@@ -10,4 +10,19 @@ if (!String.prototype.startsWith) {
 
 export function isURL(s: string): boolean {
   return s.startsWith('http://') || s.startsWith('https://') || s.startsWith('//');
+}
+
+export type DeepPartial<T> = { [P in keyof T]?: DeepPartial<T[P]> };
+
+export function mergeDeep<T>(dest: T, ...src: readonly DeepPartial<T>[]): T {
+  for (const s of src) {
+    deepMerge_(dest, s);
+  }
+  return dest;
+}
+
+function deepMerge_(dest: any, src: any) {
+  for (const property of Object.keys(src)) {
+    writeConfig(dest, property, src[property], true);
+  }
 }
