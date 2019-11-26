777da4ac4916a3d8a1625fed8d644b9a889898c5
Dominik Moritz <domoritz@gmail.com>
2019-11-26 13:31:51 -0800

    Remove document click event listener on finalize. (#280)
    Fixes #270

--- src/embed.ts
+++ src/embed.ts
@@ -335,6 +335,8 @@ async function _embed(
 
   await view.initialize(el).runAsync();
 
+  let documentClickHandler: ((this: Document, ev: MouseEvent) => void) | undefined;
+
   if (actions !== false) {
     let wrapper = div;
 
@@ -349,11 +351,12 @@ async function _embed(
 
       details.append(summary);
 
-      document.addEventListener('click', evt => {
-        if (!details.contains(evt.target as any)) {
+      documentClickHandler = (ev: MouseEvent) => {
+        if (!details.contains(ev.target as any)) {
           details.removeAttribute('open');
         }
-      });
+      };
+      document.addEventListener('click', documentClickHandler);
     }
 
     const ctrl = document.createElement('div');
@@ -435,6 +438,16 @@ async function _embed(
 
       ctrl.append(editorLink);
     }
+  }
+
+  // Wrap view.finalize to also remove event listeners from Vega-Embed.
+  if (documentClickHandler) {
+    const originalFinalize = view.finalize.bind(view);
+    view.finalize = () => {
+      document.removeEventListener('click', documentClickHandler!);
+
+      originalFinalize();
+    };
   }
 
   return { view, spec, vgSpec };
