d4af2f513be075c11036b65430d568ce7ac3732e
Dominik Moritz <domoritz@gmail.com>
2020-08-27 20:35:41 -0700

    chore: move to rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,62 @@
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
 
-export default {
-  input: "build/src/index.js",
-  output: {
-    file: "build/vega-embed.js",
-    format: "umd",
-    sourcemap: true,
-    name: "vegaEmbed",
-    globals: {
-      vega: "vega",
-      "vega-lite": "vegaLite"
-    }
+const pkg = require("./package.json");
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: "build/vega-embed.js",
+        format: "iife",
+        sourcemap: true,
+        name: "vegaEmbed",
+        globals: {
+          vega: "vega",
+          "vega-lite": "vegaLite"
+        }
+      },
+      {
+        file: "build/vega-embed.js",
+        format: "iife",
+        sourcemap: true,
+        name: "vegaEmbed",
+        globals: {
+          vega: "vega",
+          "vega-lite": "vegaLite"
+        },
+        plugins: [terser()]
+      }
+    ],
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "tsconfig.src.json"
+      }),
+      json()
+    ],
+    external: ["vega", "vega-lite"]
   },
-  plugins: [nodeResolve(), json(), commonjs()],
-  external: ["vega", "vega-lite"]
-};
+  {
+    input: "src/index.ts",
+    output: {
+      file: "build/vega-embed.module.js",
+      format: "esm",
+      sourcemap: true
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      typescript({
+        tsconfig: "tsconfig.src.json"
+      }),
+      json()
+    ],
+    external: [...Object.keys(pkg.dependencies), ...Object.keys(pkg.peerDependencies)]
+  }
+];
