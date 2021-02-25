123c1c5bdf52048d3c8a6836b0998934e1ed954b
Dominik Moritz <domoritz@gmail.com>
2021-02-25 11:14:28 +0100

    upgrade config

--- .config/beemo.ts
+++ .config/beemo.ts
@@ -1,4 +1,6 @@
-const config = {
+import { Config } from "vega-lite-dev-config";
+
+const config: Config = {
   module: 'vega-lite-dev-config',
   drivers: ['typescript', 'prettier', 'eslint', 'babel', 'jest'],
 };
