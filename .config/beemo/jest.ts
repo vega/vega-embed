552c9638f8e3f52c8ad7a28d8d8e765052d630ab
Dominik Moritz <domoritz@gmail.com>
2021-07-19 11:37:09 -0700

    chore: upgrade deps

--- .config/beemo/jest.ts
+++ .config/beemo/jest.ts
@@ -3,6 +3,7 @@ import {JestConfig} from '@beemo/driver-jest';
 const config: JestConfig = {
   testURL: 'http://localhost/',
   setupFiles: ['jest-canvas-mock'],
+  testEnvironment: 'jsdom',
 };
 
 export default config;
