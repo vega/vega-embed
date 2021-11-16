ab3fcd5db386c18fae05991602f2582836d68cdc
Dominik Moritz <domoritz@gmail.com>
2021-11-16 09:10:46 -0500

    fix: update style generation script
    fixes #792

--- build-style.sh
+++ build-style.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 printf "// generated with build-style.sh\nexport default \`" > src/style.ts
-yarn sass vega-embed.scss >> src/style.ts
+yarn -s sass vega-embed.scss >> src/style.ts
 echo "\`;" >> src/style.ts
