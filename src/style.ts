69588d3c7b07aee69eb141dff5c0b8f9a7044023
Ben Hammond <benjamin.hammond@gmail.com>
2022-01-18 19:49:14 -0700

    b

--- src/style.ts
+++ src/style.ts
@@ -46,7 +46,7 @@ export default `.vega-embed {
 .vega-embed details[open] summary {
   opacity: 0.7;
 }
-.vega-embed:hover summary, .vega-embed:focus summary {
+.vega-embed:hover summary {
   opacity: 1 !important;
   transition: opacity 0.2s ease;
 }
