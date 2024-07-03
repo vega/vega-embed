09e7f169f0824993791b7880c2518cfa0c4ec3b1
Dominik Moritz <domoritz@gmail.com>
2024-07-03 10:42:04 -0400

    fix ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -73,6 +73,7 @@ export default [
       "@typescript-eslint/no-unsafe-argument": "warn",
       "@typescript-eslint/no-unsafe-return": "warn",
       "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
       "@typescript-eslint/no-object-literal-type-assertion": "off",
       "@typescript-eslint/explicit-module-boundary-types": "off",
       "@typescript-eslint/no-namespace": "error",
