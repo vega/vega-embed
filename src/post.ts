f0f448ab74b8c95cdff6c023c550ea7f227be074
Dominik Moritz <domoritz@gmail.com>
2024-08-01 21:34:38 -0400

    style: format

--- src/post.ts
+++ src/post.ts
@@ -4,12 +4,11 @@ import {MessageData} from './types';
  * Open editor url in a new window, and pass a message.
  */
 export default function (window: Window, url: string, data: MessageData) {
-  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const editor = window.open(url)!;
   const wait = 10_000;
   const step = 250;
   const {origin} = new URL(url);
-  // eslint-disable-next-line no-bitwise
+
   let count = ~~(wait / step);
 
   function listen(evt: MessageEvent) {
